48daf097198d79751c4284bde08bfac227c0b99b
SinonJZH <sinon.jzh@gmail.com>
2021-03-28 17:51:54 +0800

    修正默认回复规则错误

--- plugins/manual/__init__.py
+++ plugins/manual/__init__.py
@@ -1,21 +1,31 @@
 import nonebot
 from .config import Config
 from nonebot.plugin import on_message, on_command
-from nonebot.rule import to_me
+from nonebot.rule import to_me, Rule
 from nonebot.typing import T_State
-from nonebot.adapters.mirai import Bot, Event
+from nonebot.adapters.mirai import Bot, Event, message
 
 global_config = nonebot.get_driver().config
 plugin_config = Config(**global_config.dict())
 
-manual = on_message(rule=to_me(), priority=99)
-help = on_command('help', priority=98)
+#定义默认回复规则
+def dafult_checker(bot: Bot, event: Event, state: T_State) -> bool:
+    event_raw = event.normalize_dict()
+    if event_raw['type'] == 'FriendMessage':
+        return True
+    if event_raw['type'] == 'GroupMessage':
+        MessageChain = event_raw['message_chain']
+        for Chain in MessageChain:
+            if Chain['type'] == 'At' and Chain['target'] == 2832089175:
+                return True
+    return False
 
+manual = on_message(rule=dafult_checker, priority=99)
+help = on_command('help', priority=98)
 
 @manual.handle()
 async def manual_handle(bot: Bot, event: Event, state: T_State):
     await manual.finish(plugin_config.about)
-
 
 @help.handle()
 async def help_handle(bot: Bot, event: Event, state: T_State):
