5f2029be329af6e24f096a77d84b4e8ce8f1d709
SinonJZH <sinon.jzh@gmail.com>
2021-03-28 15:40:12 +0800

    Update __init__.py

--- plugins/manual/__init__.py
+++ plugins/manual/__init__.py
@@ -1,6 +1,6 @@
 import nonebot
 from .config import Config
-from nonebot.plugin import on_message,on_command
+from nonebot.plugin import on_message, on_command
 from nonebot.rule import to_me
 from nonebot.typing import T_State
 from nonebot.adapters.mirai import Bot, Event
@@ -8,12 +8,14 @@ from nonebot.adapters.mirai import Bot, Event
 global_config = nonebot.get_driver().config
 plugin_config = Config(**global_config.dict())
 
-manual = on_message(rule=to_me(),priority=99)
+manual = on_message(rule=to_me(), priority=99)
 help = on_command('help', priority=98)
+
 
 @manual.handle()
 async def manual_handle(bot: Bot, event: Event, state: T_State):
     await manual.finish(plugin_config.about)
+
 
 @help.handle()
 async def help_handle(bot: Bot, event: Event, state: T_State):
